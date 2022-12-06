aa2ae93158b9093d23f36806e8f03bdb052c8847
Asa Martin <asamartn@amazon.com>
2022-12-06 09:43:11 -0500

    alt day5

--- 2022/day5-1.py
+++ 2022/day5-1.py
@@ -1,29 +1,30 @@
-rows = []
+stacks = {}
 with open("day5.stacks.data") as f:
     for line in f:
-        rows.append(line)
-stackNames = rows.pop(-1).split()
-stacks = []
-first = True
-for row in reversed(rows):
-    for idx, col in enumerate(stackNames):
-        if first:
-            stacks.append([])
-        box = row[(idx * 4) + 1].strip()
-        if box:
-            stacks[idx].append(box)
-    first = False
-print(stacks)
+        if not line.strip():
+            continue
+        for idx in range(int(len(line) / 4)):
+            col = (idx * 4) + 1
+            if idx not in stacks:
+                stacks[idx] = []
+            box = line[col].strip()
+            if box:
+                stacks[idx].append(box)
+for key in stacks.keys():
+    print(key, stacks[key])
 with open("day5.instructions.data") as f:
     for line in f:
         instructions = line.split()
-        for cnt in range(int(instructions[1])):
+        for moveCnt in range(int(instructions[1])):
             fromIdx = int(instructions[3])
             toIdx = int(instructions[5])
-            box = stacks[fromIdx - 1].pop(len(stacks[fromIdx - 1]) - 1)
-            stacks[toIdx - 1].append(box)
-print(stacks)
+            box = stacks[fromIdx - 1][0:1]
+            del stacks[fromIdx - 1][0:1]
+            box.extend(stacks[toIdx - 1])
+            stacks[toIdx - 1] = box
+for key in stacks.keys():
+    print(key, stacks[key])
 output = ""
-for stack in stacks:
-    output += stack[len(stack) - 1]
+for key in stacks:
+    output += stacks[key][0]
 print(output)
--- 2022/day5-2.py
+++ 2022/day5-2.py
@@ -1,30 +1,30 @@
-rows = []
+stacks = {}
 with open("day5.stacks.data") as f:
     for line in f:
-        rows.append(line)
-stackNames = rows.pop(-1).split()
-stacks = []
-first = True
-for row in reversed(rows):
-    for idx, col in enumerate(stackNames):
-        if first:
-            stacks.append([])
-        box = row[(idx * 4) + 1].strip()
-        if box:
-            stacks[idx].append(box)
-    first = False
-print(stacks)
+        if not line.strip():
+            continue
+        for idx in range(int(len(line) / 4)):
+            col = (idx * 4) + 1
+            if idx not in stacks:
+                stacks[idx] = []
+            box = line[col].strip()
+            if box:
+                stacks[idx].append(box)
+for key in stacks.keys():
+    print(key, stacks[key])
 with open("day5.instructions.data") as f:
     for line in f:
         instructions = line.split()
         moveCnt = int(instructions[1])
         fromIdx = int(instructions[3])
         toIdx = int(instructions[5])
-        boxes = stacks[fromIdx - 1][-moveCnt:]
-        del stacks[fromIdx - 1][-moveCnt:]
-        stacks[toIdx - 1].extend(boxes)
-print(stacks)
+        boxes = stacks[fromIdx - 1][:moveCnt]
+        del stacks[fromIdx - 1][:moveCnt]
+        boxes.extend(stacks[toIdx - 1])
+        stacks[toIdx - 1] = boxes
+for key in stacks.keys():
+    print(key, stacks[key])
 output = ""
-for stack in stacks:
-    output += stack[len(stack) - 1]
+for key in stacks:
+    output += stacks[key][0]
 print(output)

8edc3613db8e27ab8e68bb5f70d769e676c5fa7d
Asa Martin <asamartn@amazon.com>
2022-12-07 10:31:35 -0500

    day7

---
+++ 2022/day7-2.py
@@ -0,0 +1,57 @@
+fileTree = {"type": "dir", "name": "/", "children": {}, "parent": None, "size": 0}
+currentDir = fileTree
+with open("day7.data") as f:
+    for line in f:
+        line = line.strip()
+        tokens = line.split()
+        if tokens[0] == "$":
+            if tokens[1] == "cd":
+                if tokens[2] == "/":
+                    currentDir == fileTree
+                elif tokens[2] == "..":
+                    currentDir = currentDir["parent"]
+                else:
+                    currentDir = currentDir["children"][tokens[2]]
+        elif tokens[0] == "dir":
+            node = {"type": "dir", "name": tokens[1], "children": {}, "parent": currentDir, "size": 0}
+            currentDir["children"][tokens[1]] = node
+        else:
+            node = {"type": "file", "name": tokens[1], "children": {}, "parent": currentDir, "size": int(tokens[0])}
+            currentDir["children"][tokens[1]] = node
+
+def fillSize(tree):
+    for child in tree["children"].values():
+        if child["type"] == "dir":
+            tree["size"] += fillSize(child)
+        else:
+            tree["size"] += child["size"]
+    return tree["size"]
+
+fillSize(fileTree)
+
+def printTree(tree, depth = "-"):
+    print(depth, tree["name"], "(" + tree["type"] + ", size=" + str(tree["size"]) + ")")
+    for child in tree["children"].values():
+        printTree(child, " " + depth)
+
+printTree(fileTree)
+
+totalDiskSpace = 70000000
+print("total disk space", totalDiskSpace)
+updateSize = 30000000
+print("update size", updateSize)
+availableSpace = totalDiskSpace - fileTree["size"]
+print("available space", availableSpace)
+neededSpace = updateSize - availableSpace
+print("needed space", neededSpace)
+
+def findSmallestGreaterThan(tree, smallest):
+    if tree["type"] == "dir":
+        for child in tree["children"].values():
+            smallest = findSmallestGreaterThan(child, smallest)
+        if tree["size"] < smallest["size"] and tree["size"] > neededSpace:
+            smallest = tree
+    return smallest
+
+smallest = findSmallestGreaterThan(fileTree, fileTree)
+print("smallest", smallest["name"], smallest["size"])
